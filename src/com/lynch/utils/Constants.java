f6c1e44037862c7b4e15b8bff33cbb6c7cc4eb77
lynch <trouble.linky@gmail.com>
2016-09-08 10:20:44 +0800

    no message

---
+++ src/com/lynch/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.lynch.utils;
+
+/**
+ * Created by linxueqi on 2016/9/7 0007.
+ */
+public class Constants {
+    public static final String DOMAIN =  "www.lessomall.com";
+    public static final String SCHEMA =  "http";
+    public static final String URL_CATEGORY = DOMAIN + "/rest/api/v1/home/categoryList";
+    public static final String URL_SINGLE_CATEGORY = DOMAIN + "/rest/api/v1/product/productListForCategory";
+    public static final String URL_MULTIPRICE = DOMAIN + "/rest/api/v1/innershops/multiPrice";
+    public static final String URL_DETAIL = DOMAIN + "/p";
+}
